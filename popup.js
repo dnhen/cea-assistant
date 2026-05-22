1f3ced9a791a2e14dd4fd31d2495bf8997eccff4
dnhen <69449713+dnhen@users.noreply.github.com>
2026-05-22 16:44:25 +1000

    Added dropdown

--- popup.js
+++ popup.js
@@ -1,3 +1,40 @@
+// Notification Management
+const NOTIFICATION_DURATION_SECONDS = 5;
+
+const notificationElement = document.querySelector('.notification');
+const notificationIndicatorElement = notificationElement.querySelector('.activeIndicator');
+
+function pushNotification(text, type) {
+  // Reset classList
+  notificationElement.className = 'container notification hidden';
+
+  // Update notification text
+  notificationElement.querySelector('p').innerText = text;
+
+  // Add notification type indicator
+  switch (type) {
+    case 'green':
+      notificationIndicatorElement.classList.add('green');
+      break;
+    case 'red':
+      notificationIndicatorElement.classList.add('red');
+      break;
+    default:
+      notificationIndicatorElement.classList.add('green');
+      break;
+  }
+
+  // Show the notification element
+  notificationElement.classList.remove('hidden');
+
+  // Set timer to make it hide after X seconds
+  setTimeout(hideNotification, NOTIFICATION_DURATION_SECONDS * 1000);
+}
+
+const hideNotification = () => {
+  notificationElement.classList.add('hidden');
+};
+
 // Tab Management
 let activeTab = 'roll';
 
--- roll.js
+++ roll.js
@@ -11,6 +11,8 @@ fileInput.addEventListener('change', (e) => {
     const fileName = e.target.files[0].name;
     fileInputZone.querySelector('.title').innerHTML = fileName;
     fileInputZone.querySelector('.subtitle').classList.add('hidden');
+
+    pushNotification('Successfully uploaded file', 'green');
   }
 });
 
@@ -45,36 +47,46 @@ document.getElementById('markRollActionButton').addEventListener('click', async 
     // Get the current active tab
     const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
+    // Get the key to compare against
+    const keyValue = document.querySelector('#rollKeySelector').value;
+
     // Execute the script on the page, passing targetIds via the 'args' array
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       func: checkAttendance,
-      args: [targetIds],
+      args: [targetIds, keyValue],
     });
   };
 
   // Read the file as plain text
   reader.readAsText(file);
+
+  pushNotification('Successfully marked the roll', 'green');
 });
 
-function checkAttendance(targetIds) {
-  console.log('Received IDs from popup:', targetIds);
-
+function checkAttendance(targetIds, keyValue) {
   // Find all rows in the roll
   const rows = document.querySelectorAll('tr[ng-repeat*="item.Entries"]');
 
   rows.forEach((row) => {
-    // Locate the ID cell inside this specific row
-    const idCell = row.querySelectorAll('td.tablet')[2];
+    // Set the cell to compare against as the ID by default, override below based on key selector
+    let idCell = row.querySelectorAll('td.tablet')[2];
+
+    console.log(keyValue);
+
+    switch (keyValue) {
+      case 'name':
+        idCell = row.querySelectorAll('td.all')[1];
+        break;
+    }
 
     if (idCell) {
       const rowId = idCell.textContent.trim();
 
       // Check if the current row's ID exists in the target array
-      if (targetIds.includes(rowId)) {
+      if (targetIds.some((item) => rowId.includes(item))) {
         // Find the button inside the 'td.all' cell of this exact row
         const button = row.querySelector('td.all button');
-        console.log('button', button);
 
         if (button) {
           button.click();
